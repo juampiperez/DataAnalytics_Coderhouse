82c4a1faba4e448d7214ee40dc4091b5e8f6ee1a
JP <perjuanpablo@gmail.com>
2026-07-22 11:43:56 -0300

    Create modulo2_unidad1_diseno.sql

---
+++ modulo2_unidad1_diseno.sql
@@ -0,0 +1,13 @@
+CREATE TABLE clientes(
+  id_cliente INT,
+  nombre VARCHAR(100),
+  perfil_bio TEXT,
+  fecha_registro DATE
+  );
+CREATE TABLE productos(
+  id_producto INT,
+  nombre_producto VARCHAR(255),
+  precio DECIMAL(10, 2),
+  esta_activo VARCHAR(2)
+);
+--
